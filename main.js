160e2cb631d99e5af270c8612cf5dce75f974c02
Olga <kartoshkaslukom@gmail.com>
2023-04-28 19:23:55 +0200

    feat: add input symbols with virtual keyboard

--- main.js
+++ main.js
@@ -30,6 +30,17 @@ function render() {
       const key = document.createElement('li');
       key.classList.add('key');
       key.classList.add(keyValues[i]["code"]);
+      key.addEventListener('mousedown', () => {
+        if (!key.classList.contains('CapsLock')) {
+          key.classList.add('active');
+        }
+        enterSingleSymbol(key);
+      })
+      key.addEventListener('mouseup', () => {
+        if (!key.classList.contains('CapsLock')) {
+          key.classList.remove('active');
+        }
+      })
       keyboard.appendChild(key);
       for (let j = 1; j < 5; j++) {
         const symbol = document.createElement('span');
@@ -49,3 +60,39 @@ function render() {
   }
   
   render()
+
+  function enterSingleSymbol(key) {
+    const textarea = document.querySelector('.textarea');
+    textarea.focus();
+    let start = textarea.selectionStart;
+    let end = textarea.selectionEnd;
+    if (!key.classList.contains('special-key')) {
+      textarea.value = textarea.value.slice(0, start) + key.innerText + textarea.value.slice(end);
+      textarea.setSelectionRange(start + 1, end + 1);
+    } else if (key.classList.contains('Backspace')) {
+      if (start != end) {
+        textarea.value = textarea.value.slice(0, start) + textarea.value.slice(end);
+        textarea.setSelectionRange(start, start);
+      } else {
+        textarea.value = textarea.value.slice(0, start - 1) + textarea.value.slice(start);
+        textarea.setSelectionRange(start - 1, start - 1);
+      } 
+    } else if (key.classList.contains('Delete')) {
+      if (start != end) {
+        textarea.value = textarea.value.slice(0, start) + textarea.value.slice(end);
+        textarea.setSelectionRange(start, start);
+      } else {
+        textarea.value = textarea.value.slice(0, start) + textarea.value.slice(start + 1);
+        textarea.setSelectionRange(start, start);
+      } 
+    } else if (key.classList.contains('Space')) {                                   
+      textarea.value = textarea.value.slice(0, start) + ' ' + textarea.value.slice(end);
+      textarea.setSelectionRange(start + 1, end + 1);
+    } else if (key.classList.contains('Enter')) {
+      textarea.value = textarea.value.slice(0, start) + '\n' + textarea.value.slice(end);
+      textarea.setSelectionRange(start + 1, start + 1);
+    } else if (key.classList.contains('Tab')) {
+      textarea.value = textarea.value.slice(0, start) + '    ' + textarea.value.slice(end);
+      textarea.setSelectionRange(start + 4, start + 4);
+    }
+  }
